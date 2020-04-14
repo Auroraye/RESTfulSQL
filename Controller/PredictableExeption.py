80800fed521d3d5bcc6f7e33cb53b8520d10f20a
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-04-14 09:53:59 -0400

    The PredictableDuplicateColumnException and two more types for the PredictableInvalidArgumentException

--- Controller/PredictableExeption.py
+++ Controller/PredictableExeption.py
@@ -15,8 +15,21 @@ class PredictableUnknownKeyException(PredictableException):
 
 class PredictableInvalidArgumentException(PredictableException):
     def handle_me(self):
-        if self.massage == "2":
-            text = "Please chech the parentheses in your 'uniques' field."
+        if self.massage == "1":
+            text = "Please fill in the 'table' field."
+            return text
+        elif self.massage == "2":
+            text = "Only one table can be created each time, please remove other tables from the 'table' field."
+            return text
+        elif self.massage == "3":
+            text = "Please check the parentheses in your 'uniques' field."
             return text
         else:
             return self.massage
+
+class PredictableDuplicateColumnException(PredictableException):
+    def handle_me(self):
+        text = "There is at least one column in your 'columns' field duplicated, and the first one is "
+        text = text + self.massage + "."
+        return text
+
