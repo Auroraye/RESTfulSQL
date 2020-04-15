c3ad5c41977f331089febcc5fc49b51fd7465c73
Jayden <jayden_sun@outlook.com>
2020-04-15 10:27:00 -0400

    Changed quote

--- app.py
+++ app.py
@@ -14,60 +14,60 @@ load_dotenv()
 
 flask_app = Flask(__name__)
 api = Api(app=flask_app,
-          version='1.0',
-          title='Name Recorder',
-          description='Manage names of various users of the application')
+          version="1.0",
+          title="Name Recorder",
+          description="Manage names of various users of the application")
 
-flask_app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-flask_app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT')
-flask_app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-flask_app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-flask_app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+flask_app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+flask_app.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
+flask_app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+flask_app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+flask_app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(flask_app)
 current_database = ""
 
-table_space = api.namespace('table', description='Manage tables')
-metadata_space = api.namespace('metadata', description='Manage metadata')
+table_space = api.namespace("table", description="Manage tables")
+metadata_space = api.namespace("metadata", description="Manage metadata")
 tabledata_space = api.namespace(
-    'table/data', description='Manage data records')
+    "table/data", description="Manage data records")
 
 
-table_model = api.model('Table Model',
-                        {'name': fields.String(required=True,
-                                               description='Name of the person',
-                                               help='Name cannot be blank.'),
-                         'columns': fields.String(required=True),
-                         'uniques': fields.String()})
+table_model = api.model("Table Model",
+                        {"name": fields.String(required=True,
+                                               description="Name of the person",
+                                               help="Name cannot be blank."),
+                         "columns": fields.String(required=True),
+                         "uniques": fields.String()})
 
 
-@table_space.route('/')
+@table_space.route("/")
 class TableList(Resource):
-    @api.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'})
+    @api.doc(responses={200: "OK", 400: "Invalid Argument", 500: "Mapping Key Error"})
     @api.expect(table_model)
     def post(self):
         try:
-            table = request.json['name']
-            column = request.json['columns']
-            unique = request.json['uniques']
+            table = request.json["name"]
+            column = request.json["columns"]
+            unique = request.json["uniques"]
             return create_table(table, column, unique, mysql)
         except KeyError as e:
             table_space.abort(
-                500, e.__doc__, status='Could not save information', statusCode='500')
+                500, e.__doc__, status="Could not save information", statusCode="500")
         except PredictableException as e:
             table_space.abort(
-                500, e.__doc__, status=e.handle_me(), statusCode='300')
+                500, e.__doc__, status=e.handle_me(), statusCode="300")
         except Exception as e:
             table_space.abort(
-                400, e.__doc__, status='Could not save information', statusCode='400')
-        return {'result': 'Language added'}, 201
+                400, e.__doc__, status="Could not save information", statusCode="400")
+        return {"result": "Language added"}, 201
 
 
-@table_space.route('/<string:table_name>')
+@table_space.route("/<string:table_name>")
 class Table(Resource):
     def delete(self, table_name):
-        status, message, data, error = delete_table(table_name, '')
-        return {'message': message}, status
+        status, message, data, error = delete_table(table_name, "")
+        return {"message": message}, status
 
 
 @metadata_space.route("/<table_name>")
