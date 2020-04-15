4ffe83ca3c0a9064b2f2e9a20c44327b6509cb06
Yexin <xintianye@outlook.com>
2020-04-15 15:24:01 -0700

    Update MetaController.py

--- controller/MetaController.py
+++ controller/MetaController.py
@@ -1,6 +1,1 @@
-def organize_return(status, message, data, error):
-    return {"message": message}, status
 
-
-def organize_return_with_data(status, message, data, error):
-    return {"message": message, "data": data}, status
