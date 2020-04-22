518efe8a71ae313e49e83a060547de47f344caa0
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2020-04-22 13:25:25 -0400

    Add group by

--- controller/GroupController.py
+++ controller/GroupController.py
@@ -1,2 +1,31 @@
-def post_group_by(table, function, new_name, groupby, view, mysql):
-    pass
+from controller.PredictableExeption import PredictableNumberOfParameterNotMatchException
+from util.ExtractSpecialArray import check_table_field
+
+
+def post_group_by(table, function, rename, group, view, mysql):
+    check_table_field(table)
+    functions = function.split(",")
+    renames = rename.split(",")
+    if len(functions) != len(renames):
+        raise PredictableNumberOfParameterNotMatchException("functions,renames")
+
+    command = "CREATE VIEW `" + view + "` AS SELECT *"
+    i = 0
+    while i < len(functions):
+        command += ", " + functions[i] + " AS " + renames[i]
+    command += " FROM `" + table + "` GROUP BY `" + group + "`;"
+    con = mysql.connection
+    cur = con.cursor()
+    try:
+        cur.execute(command)
+        con.commit()
+        cur.close()
+        con.close()
+    except Exception as e:
+        con.rollback()
+        cur.close()
+        con.close()
+        raise e
+    status = 200
+    message = "View " + view + " has been created."
+    return status, message, None, None
