ea68991fff3b8f688e6a400b9f3b1e208416cea2
Jayden <jayden_sun@outlook.com>
2020-04-22 16:50:21 -0400

    Changed to upper case

--- app.py
+++ app.py
@@ -42,9 +42,9 @@ tabledata_space = api.namespace("table/data", description="Manage data records")
 metadata_space = api.namespace("metadata", description="Manage metadata")
 uniquekey_space = api.namespace("metadata/uniquekey", description="Manage unique key")
 foreignkey_space = api.namespace("metadata/foreignkey", description="Manage foreign key")
-union_space = api.namespace("union", description="get a union of two table")
-groupby_space = api.namespace("groupby", description="apply grouping and statistic functions to a table")
-join_space = api.namespace("join", description="get a join of tables")
+union_space = api.namespace("union", description="Get a union of two table")
+groupby_space = api.namespace("groupby", description="Apply grouping and statistic functions to a table")
+join_space = api.namespace("join", description="Get a join of tables")
 
 # Here starts the table module.
 table_model = api.model("Table Model",
